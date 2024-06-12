394df30bc12d685f6f599ff338c5326a35f90dcb
dominog125 <76757727+dominog125@users.noreply.github.com>
2024-06-12 18:05:41 +0200

    Delete mysite directory

--- mysite/Teststrona/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class gigachad(models.Model):
-    imie = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.imie
